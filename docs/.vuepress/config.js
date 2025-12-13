13ed73d73fc3d3f2cb8939b4f48809b1767b1174
shi0rik0 <shi0rik0@outlook.com>
2025-12-13 09:40:35 +0800

    Upload contents

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,72 @@
-import { defaultTheme } from '@vuepress/theme-default'
-import { defineUserConfig } from 'vuepress'
-import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from "@vuepress/theme-default";
+import { defineUserConfig } from "vuepress";
+import { viteBundler } from "@vuepress/bundler-vite";
 
 export default defineUserConfig({
-  lang: 'en-US',
+  lang: "zh-TW",
 
-  title: 'VuePress',
-  description: 'My first VuePress Site',
+  title: "臺灣閩南語按呢寫",
+  description: "臺灣閩南語按呢寫綫上閲覽",
 
   theme: defaultTheme({
-    logo: 'https://vuejs.press/images/hero.png',
+    logo: "https://vuejs.press/images/hero.png",
 
-    navbar: ['/', '/get-started'],
+    navbar: [
+      {
+        text: "首頁",
+        link: "/",
+      },
+      {
+        text: "正文",
+        link: "/contents/",
+      },
+    ],
+    sidebar: {
+      "/contents/": [
+        {
+          text: "目錄",
+          link: "/contents/",
+        },
+        {
+          text: "第 265 - 299 期",
+          prefix: "/contents/",
+          children: Array.from({ length: 35 }, (_, i) => ({
+            text: `第 ${265 + i} 期`,
+            link: `annesia${265 + i}`,
+          })),
+          collapsible: true,
+        },
+        {
+          text: "第 300 - 399 期",
+          prefix: "/contents/",
+          children: Array.from({ length: 100 }, (_, i) => ({
+            text: `第 ${300 + i} 期`,
+            link: `annesia${300 + i}`,
+          })),
+          collapsible: true,
+        },
+        {
+          text: "第 400 - 499 期",
+          prefix: "/contents/",
+          children: Array.from({ length: 100 }, (_, i) => ({
+            text: `第 ${400 + i} 期`,
+            link: `annesia${400 + i}`,
+          })),
+          collapsible: true,
+        },
+        {
+          text: "第 500 - 544 期",
+          prefix: "/contents/",
+          children: Array.from({ length: 45 }, (_, i) => ({
+            text: `第 ${500 + i} 期`,
+            link: `annesia${500 + i}`,
+          })),
+          collapsible: true,
+        },
+      ],
+    },
+    sidebarDepth: 1,
   }),
 
   bundler: viteBundler(),
-})
+});
